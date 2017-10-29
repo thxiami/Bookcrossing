35bc6a1b7bac9cb715e628e96b3adf9f24f5050f
thxiami <thxiami@gmail.com>
2017-10-30 01:47:39 +0800

    fix: stmp bug fixed

--- config.py
+++ config.py
@@ -7,16 +7,22 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #MAIL_SERVER = 'smtp.googlemail.com'
+    # MAIL_SERVER = 'smtp.qq.com'
     MAIL_SERVER = 'smtp.163.com'
+
     #MAIL_PORT = 587
     MAIL_PORT = 994  # 163 的SSL SMTP 端口
+    # MAIL_PORT = 465  # qq 的 SSL SMTP 端口
+
     # MAIL_USE_TLS = True
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[870587064]'
-    FLASKY_MAIL_SENDER = 'BookCrossing Admin <870587064@qq.com>'
-    FLASKY_ADMIN = os.environ.get('BOOKCROSSING_ADMIN')
+
+    FLASKY_MAIL_SUBJECT_PREFIX = '[BookCrossing Admin]'
+    FLASKY_MAIL_SENDER = 'BookCrossing Admin <tjulyw2015@163.com>'
+    # FLASKY_ADMIN = os.environ.get('BOOKCROSSING_ADMIN')
+    FLASKY_ADMIN = 'tjulyw2015@163.com'
 
     @staticmethod
     def init_app(app):
